393ec4772654aaa0347eaf4c18ebae76e3484cef
hakan <tasbeyhakan@gmail.com>
2023-11-21 20:11:41 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let isRunning = false //? Kronometre calisma durumu
+let startTime; //? Baslangic zamani 
+let elapsedTime = 0 //? Kronometre de gecen sure  
+let intervalID; //? setInterval tarafindan dondurulen ID
+
+
+
